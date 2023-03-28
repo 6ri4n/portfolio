63ab6f06fe43acf54c89ca785d2da598eec556df
brian <bri4n.huang@gmail.com>
2023-03-27 17:01:20 -0700

    projects section animations

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", () => {
   createAboutSectionObserver();
+  createProjectsSectionObserver();
+  changeProjectTitleColor();
 });
 
 function createAboutSectionObserver() {
@@ -14,7 +16,7 @@ function createAboutSectionObserver() {
   let observer = new IntersectionObserver(animateSlideLeft, {
     root: null,
     rootMargin: "0px",
-    threshold: 0.05,
+    threshold: 0.001,
   });
 
   let aboutP1 = document.querySelector("#about-part1");
@@ -24,4 +26,59 @@ function createAboutSectionObserver() {
   observer.observe(aboutP1);
   observer.observe(aboutP2);
   observer.observe(aboutP3);
+}
+
+function createProjectsSectionObserver() {
+  function animateSlideUp(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (entry.target.id === "project-h2") {
+          let project1 = document.querySelector("#project-1");
+          project1.classList.add("slide-up-2");
+        } else if (entry.target.id === "project-1") {
+          let projectH2 = document.querySelector("#projects h2");
+          projectH2.classList.add("slide-up-2");
+        }
+        entry.target.classList.add("slide-up-2");
+      }
+    });
+  }
+
+  let observer = new IntersectionObserver(animateSlideUp, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.001,
+  });
+
+  let projectH2 = document.querySelector("#project-h2");
+  let project1 = document.querySelector("#project-1");
+  let project2 = document.querySelector("#project-2");
+  let project3 = document.querySelector("#project-3");
+  let project4 = document.querySelector("#project-4");
+  let project5 = document.querySelector("#project-5");
+  let project6 = document.querySelector("#project-6");
+
+  observer.observe(projectH2);
+  observer.observe(project1);
+  observer.observe(project2);
+  observer.observe(project3);
+  observer.observe(project4);
+  observer.observe(project5);
+  observer.observe(project6);
+}
+
+function changeProjectTitleColor() {
+  let projectItem = document.querySelectorAll(".project-item");
+
+  for (let project of projectItem) {
+    project.addEventListener("mouseover", () => {
+      let projectTitle = project.firstElementChild.children[1];
+      projectTitle.style.color = "var(--main-color)";
+    });
+
+    project.addEventListener("mouseout", () => {
+      let projectTitle = project.firstElementChild.children[1];
+      projectTitle.style.color = "black";
+    });
+  }
 }
